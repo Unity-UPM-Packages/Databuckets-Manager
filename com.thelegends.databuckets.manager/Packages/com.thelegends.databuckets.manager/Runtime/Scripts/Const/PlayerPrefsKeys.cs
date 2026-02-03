e19d9e3b92c50c0786de711f25e150475f0b3b4c
QuangHaiVu <vuquanghai0108@gmail.com>
2026-02-03 15:00:54 +0700

    basic Init

---
+++ com.thelegends.databuckets.manager/Packages/com.thelegends.databuckets.manager/Runtime/Scripts/Const/PlayerPrefsKeys.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+namespace com.thelegends.databuckets.manager
+{
+    public class PlayerPrefsKeys : MonoBehaviour
+    {
+        // Start is called once before the first execution of Update after the MonoBehaviour is created
+        void Start()
+        {
+        
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+        
+        }
+    }
+}
---
+++ com.thelegends.databuckets.manager/Packages/com.thelegends.databuckets.manager/Runtime/Scripts/Settings/DatabucketsSettings.cs
@@ -0,0 +1,43 @@
+using UnityEngine;
+
+namespace TheLegends.Base.Databuckets
+{
+    public class DatabucketsSettings : ScriptableObject
+    {
+        public const string ResDir = "Assets/TripSoft/Databuckets/Resources";
+        public const string FileName = "DatabucketsSettingsAsset";
+        public const string FileExtension = ".asset";
+
+        private static DatabucketsSettings _instance;
+        public static DatabucketsSettings Instance
+        {
+            get
+            {
+                if (_instance != null)
+                {
+                    return _instance;
+                }
+
+                _instance = Resources.Load<DatabucketsSettings>(FileName);
+                return _instance;
+            }
+        }
+
+        private string api_endpoint = "";
+        private string api_key = "";
+
+        public string APIEndpoint
+        {
+            get { return api_endpoint; }
+            set { api_endpoint = value; }
+        }
+
+        public string APIKey
+        {
+            get { return api_key; }
+            set { api_key = value; }
+        }
+
+        public bool enableExxceptionLogTracking = true;
+    }
+}
