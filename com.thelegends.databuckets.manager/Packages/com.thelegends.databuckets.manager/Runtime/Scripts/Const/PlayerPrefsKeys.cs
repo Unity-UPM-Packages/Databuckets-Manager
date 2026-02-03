9cf794f4feac452140cc5aff101f5ea0a1c06b45
QuangHaiVu <vuquanghai0108@gmail.com>
2026-02-03 17:11:48 +0700

    aaaaa

--- com.thelegends.databuckets.manager/Packages/com.thelegends.databuckets.manager/Runtime/Scripts/Const/PlayerPrefsKeys.cs
+++ com.thelegends.databuckets.manager/Packages/com.thelegends.databuckets.manager/Runtime/Scripts/Const/PlayerPrefsKeys.cs
@@ -1,19 +1,19 @@
 using UnityEngine;
 
-namespace com.thelegends.databuckets.manager
+namespace TheLegends.Base.Databuckets
 {
     public class PlayerPrefsKeys : MonoBehaviour
     {
         // Start is called once before the first execution of Update after the MonoBehaviour is created
         void Start()
         {
-        
+
         }
 
         // Update is called once per frame
         void Update()
         {
-        
+
         }
     }
 }
