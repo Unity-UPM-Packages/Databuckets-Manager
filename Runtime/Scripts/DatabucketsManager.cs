23b1e851257c67bf513dd59e3ad8f9ff7f802f57
QuangHaiVu <vuquanghai0108@gmail.com>
2026-02-04 10:17:20 +0700

    USE_DATABUCKETS define symbol

--- Runtime/Scripts/DatabucketsManager.cs
+++ Runtime/Scripts/DatabucketsManager.cs
@@ -9,6 +9,7 @@ namespace TheLegends.Base.Databuckets
     {
         public void Init()
         {
+#if USE_DATABUCKETS
             DatabucketsTracker.Init(DatabucketsSettings.Instance.APIEndpoint, DatabucketsSettings.Instance.APIKey);
 
             if (DatabucketsSettings.Instance.enableExxceptionLogTracking)
@@ -21,25 +22,31 @@ namespace TheLegends.Base.Databuckets
             }
 
             LoadCommonPropertiesFromPrefs();
+#endif
         }
 
         public void SetCommonProperty(string key, object value)
         {
+#if USE_DATABUCKETS
             DatabucketsTracker.SetCommonProperty(key, value);
             UpdateCachedProperty(key, value);
+#endif
         }
 
         public void SetCommonProperties(Dictionary<string, object> properties)
         {
+#if USE_DATABUCKETS
             DatabucketsTracker.SetCommonProperties(properties);
             foreach (var property in properties)
             {
                 UpdateCachedProperty(property.Key, property.Value);
             }
+#endif
         }
 
         public void LoadCommonPropertiesFromPrefs()
         {
+#if USE_DATABUCKETS
             var properties = new Dictionary<string, object>();
             var fields = typeof(DefaultProperties).GetFields(System.Reflection.BindingFlags.Public | System.Reflection.BindingFlags.Static | System.Reflection.BindingFlags.FlattenHierarchy);
 
@@ -63,10 +70,12 @@ namespace TheLegends.Base.Databuckets
             {
                 DatabucketsTracker.SetCommonProperties(properties);
             }
+#endif
         }
 
         private void UpdateCachedProperty(string key, object value)
         {
+#if USE_DATABUCKETS
             if (key == DefaultProperties.CURRENT_LEVEL || key.EndsWith("_n"))
             {
                 UnityEngine.PlayerPrefs.SetInt(key, Convert.ToInt32(value));
@@ -77,16 +86,21 @@ namespace TheLegends.Base.Databuckets
             }
 
             UnityEngine.PlayerPrefs.Save();
+#endif
         }
 
         public void RecordEvent(string eventName, Dictionary<string, object> properties = null)
         {
+#if USE_DATABUCKETS
             DatabucketsTracker.Record(eventName, properties);
+#endif
         }
 
         public void RecordEventWithTiming(string eventName, Dictionary<string, object> properties, string timingProp, string startEvent)
         {
+#if USE_DATABUCKETS
             DatabucketsTracker.RecordWithTiming(eventName, properties, timingProp, startEvent);
+#endif
         }
 
     }
